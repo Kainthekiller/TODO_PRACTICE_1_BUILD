afb8d13b4fbf67deb23edaab961b0795a23f544a
kainthekiller <phillipyates12@hotmail.com>
2022-10-24 09:55:10 -0500

    First

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import GetAllItems from "./Component/Items";
+
+function App() {
+  return (
+    <div className="App">
+        <GetAllItems/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Items.js
@@ -0,0 +1,23 @@
+//Summary This JS gets all the from  the list
+
+import {Button} from "@mui/material";
+
+
+function GetAllItems()
+{
+    //Use State
+
+    //Use Effect
+
+    //Custom Methods
+
+
+    //Main
+    return (
+        <>
+            <Button onClick={() => {console.log("Button in all was clicked")}}>Get All Items</Button>
+        </>
+    )
+}
+
+export default GetAllItems
